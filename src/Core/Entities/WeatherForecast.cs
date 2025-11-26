18adf097ecf47db78fcfae91c3fc56ab9e4544a7
Altug Celik <32844205+altugcelik@users.noreply.github.com>
2025-11-26 14:17:07 +0300

    .

--- src/Core/Entities/WeatherForecast.cs
+++ src/Core/Entities/WeatherForecast.cs
@@ -2,8 +2,8 @@ namespace Core.Entities
 {
     public class WeatherForecast
     {
-        public string City { get; set; }
+        public string? City { get; set; }
         public int Temperature { get; set; }
-        public string Condition { get; set; }
+        public string? Condition { get; set; }
     }
 }
